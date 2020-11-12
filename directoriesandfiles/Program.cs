8daa61a20125d90f976b87c749353203a831e66a
Opilane TTHK <opilane@tthk.ee>
2020-11-12 13:13:07 +0200

    f

---
+++ directoriesandfiles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace directoriesandfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string rootpath = @"C:\Users\opilane\sample";
+            string[] allDirectories = Directory.GetDirectories(rootpath);
+            ReadArry(allDirectories);
+
+            string[] includenestedDirectories = Directory.GetDirectories(rootpath, "*", SearchOption.AllDirectories);
+     
+           
+        }
+        public static void ReadArry(string[] array)
+        { 
+        foreach(string line in array )
+        }
+
+
+    }
+}
---
+++ writefilesanddirectories/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace writefilesanddirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\opilane\sample.txt";
+            string directorypath = @"C:\Users\opilane\sample";
+
+            string[] files = Directory.GetFiles(directorypath, "*.*", SearchOption.AllDirectories);
+
+        }
+    }
+}
